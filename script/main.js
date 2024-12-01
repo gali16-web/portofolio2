8fe3ba12327087f1ce9c3cb6e5562501d87c3f55
gali16-web <muhammadgalialmahdi@gmail.com>
2024-12-01 11:49:59 +0700

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,105 @@
+/* ==================== 
+  toggle icon navbar 
+====================== */
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+// Splash screen atau intro
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.intro-header');
+let logoSpan = document.querySelectorAll('.introLogo');
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    setTimeout(() => {
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.add('active');
+            }, (idx + 1) * 400);
+        });
+    });
+
+    setTimeout(() => {
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.remove('active');
+                span.classList.add('fade');
+            }, (idx + 1) * 50);
+        });
+    }, 1700);
+
+    setTimeout(() => {
+        intro.style.top = '-100vh';
+    }, 2000);
+
+});
+
+/* =========================== 
+    Scroll Section Active Link 
+============================= */
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  /* ========================
+      Sticky Navbar 
+=========================== */
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /* ===================== 
+    Menu Icon Navbar 
+====================== */
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+/* ===================== 
+    Scroll Reveal 
+====================== */
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+/* ===================== 
+    Typed Js 
+====================== */
+const typedEfek = new Typed(".multiple-text", {
+  strings: ["Mahasiswa", "Computer Technician", "Handphone Technician", "Printer Technician", "Junior Web Devoloper ", "Pramuniaga"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 100,
+  loop: true,
+});
